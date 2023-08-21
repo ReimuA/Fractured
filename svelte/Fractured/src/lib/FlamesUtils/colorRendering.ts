f76f70e762354db57d5a5c1116443ee44bba05c4
thibault <thibault.dolley@gmail.com>
2023-08-21 11:34:04 +0200

    Removed code duplciation in color rendering

--- svelte/Fractured/src/lib/FlamesUtils/colorRendering.ts
+++ svelte/Fractured/src/lib/FlamesUtils/colorRendering.ts
@@ -1,7 +1,14 @@
 import { defaultRenderMode, structuralPaletteRenderMode, type Flames } from "./Flames"
-import { mix } from "./mathu"
+import { mix, type Color } from "./mathu"
 import { type ColorPalette, colorFromPalette } from "./palette"
 import type { RenderData } from "./render"
+
+function mixColor(pixels: Uint8ClampedArray, idx: number, c: Color) {
+	pixels[idx + 0] = mix(pixels[idx + 0], 255 * c.r, .25)
+	pixels[idx + 1] = mix(pixels[idx + 1], 255 * c.g, .25)
+	pixels[idx + 2] = mix(pixels[idx + 2], 255 * c.b, .25)
+	pixels[idx + 3] = mix(pixels[idx + 3], 255, .25)
+}
 
 function paletteStructuralColoring(renderData: RenderData, p: ColorPalette) {
 	const heatmap = renderData.heatmap
@@ -13,11 +20,7 @@ function paletteStructuralColoring(renderData: RenderData, p: ColorPalette) {
 		if (heatmap[i] < 1) continue
 
 		const c = colorFromPalette(p, paletteAccumulator[i])
-
-		pixels[i * 4 + 0] = mix(pixels[i * 4 + 0], 255 * c.r, .25)
-		pixels[i * 4 + 1] = mix(pixels[i * 4 + 1], 255 * c.g, .25)
-		pixels[i * 4 + 2] = mix(pixels[i * 4 + 2], 255 * c.b, .25)
-		pixels[i * 4 + 3] = mix(pixels[i * 4 + 3], 255, .25)
+		mixColor(pixels, i * 4, c)
 	}
 	return pixels
 }
@@ -30,14 +33,13 @@ function colorStructuralColoring(renderData: RenderData) {
 		pixels[i * 4 + 3] = mix(pixels[i * 4 + 3], 255, .25)
 		if (heatmap[i] < 1) continue
 
-		const r = colorAccumulator[i * 3]
-		const g = colorAccumulator[i * 3 + 1]
-		const b = colorAccumulator[i * 3 + 2]
+		const c = {
+			r: colorAccumulator[i * 3],
+			g: colorAccumulator[i * 3 + 1],
+			b: colorAccumulator[i * 3 + 2],
+		}
 
-		pixels[i * 4 + 0] = mix(pixels[i * 4 + 0], 255 * r, .25)
-		pixels[i * 4 + 1] = mix(pixels[i * 4 + 1], 255 * g, .25)
-		pixels[i * 4 + 2] = mix(pixels[i * 4 + 2], 255 * b, .25)
-		pixels[i * 4 + 3] = mix(pixels[i * 4 + 3], 255, .25)
+		mixColor(pixels, i * 4, c)
 	}
 	return pixels
 }
@@ -54,19 +56,16 @@ function defaultColoring(renderData: RenderData, p: ColorPalette) {
 		const density = Math.log10(heatmap[i])
 		const c = colorFromPalette(p, density / maxDensity)
 
-		pixels[i * 4 + 0] = mix(pixels[i * 4 + 0], 255 * c.r, .25)
-		pixels[i * 4 + 1] = mix(pixels[i * 4 + 1], 255 * c.g, .25)
-		pixels[i * 4 + 2] = mix(pixels[i * 4 + 2], 255 * c.b, .25)
-		pixels[i * 4 + 3] = 255
+		mixColor(pixels, i * 4, c)
 	}
 	return pixels
 }
 
 export function updateFlamesColor(flames: Flames, renderData: RenderData) {
-    if (flames.renderMode === defaultRenderMode)
-        defaultColoring(renderData, flames.namedPalette.palette)
-    else if (flames.renderMode === structuralPaletteRenderMode)
-        paletteStructuralColoring(renderData, flames.namedPalette.palette);
-    else
-        colorStructuralColoring(renderData);
+	if (flames.renderMode === defaultRenderMode)
+		defaultColoring(renderData, flames.namedPalette.palette)
+	else if (flames.renderMode === structuralPaletteRenderMode)
+		paletteStructuralColoring(renderData, flames.namedPalette.palette);
+	else
+		colorStructuralColoring(renderData);
 }
