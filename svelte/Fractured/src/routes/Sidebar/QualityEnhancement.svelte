249746228fc096c06cb698e47a283191754fe460
thibault <thibault.dolley@gmail.com>
2023-08-29 18:32:46 +0200

    [FEA]Isolated Quality enhancement option

---
+++ svelte/Fractured/src/routes/Sidebar/QualityEnhancement.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import type { DensityEstimation } from "$lib/FlamesUtils/Flames";
+	import { flamesStore } from "../stores";
+
+	let gammaCorrection: number = 0.454545
+	let densityEstimation = false;
+	let antialiased = false;
+	let minSigma = 0
+	let maxSigma = 3
+    
+
+	function updateDensityEstimation() {
+		let dEstimation: DensityEstimation | null = null
+
+		if (densityEstimation) {
+			dEstimation = {minSigma: 0, maxSigma: 3}
+		}
+
+		flamesStore.update((value) => {
+			value.flames.densityEstimation = dEstimation,
+			value.resetType = 'none'
+
+			return value
+		})
+	}
+
+</script>
+
+<p class="pt-12 pl-6 text-white">Quality enhancement</p>
+<label class="block">
+    <span class="ml-12 p-1 text-white">Gamma correction</span>
+    <input
+        bind:value={gammaCorrection}
+        on:change={() => flamesStore.update(value => {
+            value.flames.gammaCorrection = gammaCorrection
+            value.resetType = 'none'
+            return value
+        })}
+        type="number"
+        min={0.1}
+        class="round-r-4 text-white bg-slate-900  w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
+    />
+</label>
+<label class="block">
+    <input
+        bind:checked={antialiased}
+        on:change={() =>
+            flamesStore.update((value) => {
+                value.flames.antialiasing = antialiased
+                value.resetType = 'none'
+                return value
+            })}
+        type="checkbox"
+        class="round-r-4 bg-slate-900 ml-12 p-1 mt-4 border-slate-300 border-2 rounded"
+    />
+    <span class="text-white">Anti aliasing</span>
+</label>
+<label class="block">
+    <input
+        bind:checked={densityEstimation}
+        on:change={updateDensityEstimation}
+        type="checkbox"
+        class="round-r-4 bg-slate-900 ml-12 p-1 mt-4 border-slate-300 border-2 rounded"
+    />
+    <span class="text-white">Density estimation</span>
+</label>
+
+<label class="block">
+    <span class="ml-12 p-1 text-white">Min sigma</span>
+    <input
+        bind:value={minSigma}
+        on:change={updateDensityEstimation}
+        type="number"
+        min="0"
+        max={maxSigma - 1}
+        class="round-r-4 text-white bg-slate-900 w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
+    />
+</label>
+<label class="block">
+    <span class="ml-12 p-1 text-white">Max sigma</span>
+    <input
+        bind:value={maxSigma}
+        on:change={updateDensityEstimation}
+        type="number"
+        min={minSigma + 1}
+        class="round-r-4 text-white bg-slate-900  w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
+    />
+</label>
--- svelte/Fractured/src/routes/Sidebar/Sidebar.svelte
+++ svelte/Fractured/src/routes/Sidebar/Sidebar.svelte
@@ -4,16 +4,9 @@
 	import ColorationOptions from './ColorationOptions.svelte';
 	import SpaceWarping from './SpaceWarping.svelte';
 	import type { DensityEstimation } from '$lib/FlamesUtils/Flames';
+	import QualityEnhancement from './QualityEnhancement.svelte';
 
 	export let open = false;
-
-	let antialiased = false;
-	let densityEstimation = false;
-	let gammaCorrection: number = 0.454545
-	let minSigma = 0
-	let maxSigma = 3
-	// HTML ref
-	let imageLink: HTMLAnchorElement | undefined;
 
 	let metadataHref = ''
 	let metadataName = 'flames.metadata.json'
@@ -32,22 +25,6 @@
 		metadataHref = URL.createObjectURL(blob);
 		metadataName = 'flames.metadata.json';
 	}
-
-	function updateDensityEstimation() {
-		let dEstimation: DensityEstimation | null = null
-
-		if (densityEstimation) {
-			dEstimation = {minSigma: 0, maxSigma: 3}
-		}
-
-		flamesStore.update((value) => {
-			value.flames.densityEstimation = dEstimation,
-			value.resetType = 'none'
-
-			return value
-		})
-	}
-
 	function downloadImage() {
 		if (!flamesCanvas) return;
 		const link = document.createElement('a');
@@ -67,66 +44,7 @@
 		<button class="text-white block" on:click={() => downloadImage()}>Image</button>
 	</nav>
 
-	<p class="pt-12 pl-6 text-white">Quality enhancement</p>
-	<label class="block">
-		<span class="ml-12 p-1 text-white">Gamma correction</span>
-		<input
-			bind:value={gammaCorrection}
-			on:change={() => flamesStore.update(value => {
-				value.flames.gammaCorrection = gammaCorrection
-				value.resetType = 'none'
-				return value
-			})}
-			type="number"
-			min={0.1}
-			class="round-r-4 text-white bg-slate-900  w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
-		/>
-	</label>
-	<label class="block">
-		<input
-			bind:checked={antialiased}
-			on:change={() =>
-				flamesStore.update((value) => {
-					value.flames.antialiasing = antialiased
-					value.resetType = 'none'
-					return value
-				})}
-			type="checkbox"
-			class="round-r-4 bg-slate-900 ml-12 p-1 mt-4 border-slate-300 border-2 rounded"
-		/>
-		<span class="text-white">Anti aliasing</span>
-	</label>
-	<label class="block">
-		<input
-			bind:checked={densityEstimation}
-			on:change={updateDensityEstimation}
-			type="checkbox"
-			class="round-r-4 bg-slate-900 ml-12 p-1 mt-4 border-slate-300 border-2 rounded"
-		/>
-		<span class="text-white">Density estimation</span>
-	</label>
-
-	<label class="block">
-		<span class="ml-12 p-1 text-white">Min sigma</span>
-		<input
-			bind:value={minSigma}
-			on:change={updateDensityEstimation}
-			type="number"
-			min="0"
-			max={maxSigma - 1}
-			class="round-r-4 text-white bg-slate-900 w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
-		/>
-	</label>
-	<label class="block">
-		<span class="ml-12 p-1 text-white">Max sigma</span>
-		<input
-			bind:value={maxSigma}
-			on:change={updateDensityEstimation}
-			type="number"
-			min={minSigma + 1}
-			class="round-r-4 text-white bg-slate-900  w-16 pl-1 mt-4 border-slate-300 border-2 rounded"
-		/>
-	</label>
+	<QualityEnhancement />
 	<SpaceWarping />
 	<ColorationOptions />
 	<VariationSelector />
